9c7471458d65592c90c1c888e771e278f3fec47d
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-03-03 19:15:28 +0530

    gallery redesign

--- src/scripts/Home.js
+++ src/scripts/Home.js
@@ -10,10 +10,11 @@ function Home() {
 					Seeking challenging and reputed career opportunities in web development, specifically in front-end development. where I can apply my skills and knowledge to develop and captivating user interfaces.
 				</p>
 				<p className='bio oswald-font'>
-					<span>lorem</span>
-					<span>lorem</span>
-					<span>lorem</span>
-					<span>lorem</span>
+					<span>Proficiency</span>
+					<span>Problem-Solving</span>
+					<span>Collaboration</span>
+					<span>Passion</span>
+					<span>Adaptability</span>
 				</p>
 				<a href="#work" className='btn btn-outline-secondary btn-md'>See Work</a>
 			</div>
